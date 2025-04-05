3f1b228bdf9fed3c8d5a341ec52349594777475a
antoninVeyre12 <antonin.veyre@iut-rodez.fr>
2025-04-05 14:57:57 +0200

    écritures des nouvelles vue suite réunion cient

--- script.sql
+++ script.sql
@@ -1,43 +1,58 @@
-
--- Créeation de la vue permettant de récupérer la somme des médicaments prescrits par laboratoire
-CREATE VIEW vente_par_labo
-AS 
-SELECT sum(qte), SAE_S6_2025.cis.titulaires
-FROM SAE_S6_2025.ordonnancelignes
-INNER JOIN SAE_S6_2025.ciscip
-ON SAE_S6_2025.ordonnancelignes.id_ciscip = SAE_S6_2025.ciscip.id
-INNER JOIN SAE_S6_2025.cis 
-ON SAE_S6_2025.ciscip.code_cis = SAE_S6_2025.cis.code_cis
-GROUP BY SAE_S6_2025.cis.titulaires
-ORDER BY sum(qte) DESC
-
--- Création de la vue permettant de récupérer la somme des médicaments prescrits 
-CREATE VIEW vente_medicaments
-AS 
-SELECT sum(qte), SAE_S6_2025.cis.code_cis, SAE_S6_2025.cis.denomination
-FROM SAE_S6_2025.ordonnancelignes
-INNER JOIN SAE_S6_2025.ciscip
-ON SAE_S6_2025.ordonnancelignes.id_ciscip = SAE_S6_2025.ciscip.id
-INNER JOIN SAE_S6_2025.cis 
-ON SAE_S6_2025.ciscip.code_cis = SAE_S6_2025.cis.code_cis
-GROUP BY SAE_S6_2025.cis.code_cis
-ORDER BY sum(qte) DESC
-
--- Requête qui permet de récupérer les laboratoires a qui on acheter la plus grande somme de médicaments qu'on a prescrit
-CREATE VIEW achat_laboratoire_argent
+CREATE VIEW medicaments_prescrits_par_mois
 AS
-SELECT SUM(prix_medicament_a * qte) AS prix_total, SAE_S6_2025.cis.titulaires
+SELECT 
+    SAE_S6_2025.cis.denomination, 
+    qte AS nombre_prescrit,
+    YEAR(SAE_S6_2025.ordonnance.date_prescription) AS annee,
+    MONTH(SAE_S6_2025.ordonnance.date_prescription) AS mois
 FROM SAE_S6_2025.ordonnancelignes
+INNER JOIN SAE_S6_2025.ordonnance
+    ON SAE_S6_2025.ordonnance.id = SAE_S6_2025.ordonnancelignes.id_ordonnance
 INNER JOIN SAE_S6_2025.ciscip
-ON SAE_S6_2025.ordonnancelignes.id_ciscip = SAE_S6_2025.ciscip.id
-INNER JOIN SAE_S6_2025.cis 
-ON SAE_S6_2025.ciscip.code_cis = SAE_S6_2025.cis.code_cis
-WHERE prix_medicament_a IS NOT NULL AND prix_medicament_a <> ''
-GROUP BY SAE_S6_2025.cis.titulaires
-ORDER BY prix_total DESC
+    ON SAE_S6_2025.ciscip.id = SAE_S6_2025.ordonnancelignes.id_ciscip
+INNER JOIN SAE_S6_2025.cis
+    ON SAE_S6_2025.ciscip.code_cis = SAE_S6_2025.cis.code_cis
+GROUP BY 
+    YEAR(SAE_S6_2025.ordonnance.date_prescription), 
+    MONTH(SAE_S6_2025.ordonnance.date_prescription),
+    SAE_S6_2025.cis.code_cis
+ORDER BY nombre_prescrit DESC
 
--- Vue retournant les médicaments les plus vendus par ville
-CREATE VIEW medicament_vendu_par_ville
+CREATE VIEW somme_commandee_labo
+AS 
+SELECT 
+    SAE_S6_2025.cis.titulaires AS laboratoire, 
+    SUM(SAE_S6_2025.ciscip.prix_medicament_a) AS somme
+FROM SAE_S6_2025.ordonnancelignes
+INNER JOIN SAE_S6_2025.ordonnance
+    ON SAE_S6_2025.ordonnance.id = SAE_S6_2025.ordonnancelignes.id_ordonnance
+INNER JOIN SAE_S6_2025.ciscip
+    ON SAE_S6_2025.ciscip.id = SAE_S6_2025.ordonnancelignes.id_ciscip
+INNER JOIN SAE_S6_2025.cis
+    ON SAE_S6_2025.ciscip.code_cis = SAE_S6_2025.cis.code_cis
+GROUP BY 
+    SAE_S6_2025.cis.titulaires
+ORDER BY somme DESC
+LIMIT 10
+
+CREATE VIEW quantite_commandee_labo
+AS
+SELECT 
+    SAE_S6_2025.cis.titulaires AS laboratoire, 
+    SUM(qte) AS quantite
+FROM SAE_S6_2025.ordonnancelignes
+INNER JOIN SAE_S6_2025.ordonnance
+    ON SAE_S6_2025.ordonnance.id = SAE_S6_2025.ordonnancelignes.id_ordonnance
+INNER JOIN SAE_S6_2025.ciscip
+    ON SAE_S6_2025.ciscip.id = SAE_S6_2025.ordonnancelignes.id_ciscip
+INNER JOIN SAE_S6_2025.cis
+    ON SAE_S6_2025.ciscip.code_cis = SAE_S6_2025.cis.code_cis
+GROUP BY 
+    SAE_S6_2025.cis.titulaires
+ORDER BY quantite DESC
+LIMIT 10
+
+CREATE VIEW medicaments_prescrits_par_ville
 AS
 SELECT sum(qte) as quantite, denomination, TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(adresse_postale, ',', -2), ',', 1), ' ',-1)) AS ville
 FROM SAE_S6_2025.ordonnancelignes
@@ -52,8 +67,7 @@ ON SAE_S6_2025.patients.numero_securite_sociale = SAE_S6_2025.ordonnance.numero_
 GROUP BY ville, SAE_S6_2025.ciscip.code_cis
 ORDER BY quantite DESC
 
--- Vue retournant la part d emedicaments génériques et la part de médicaments de marque
-CREATE VIEW part_medicament_generiques
+CREATE VIEW repartion_medicament_generique
 AS
 SELECT sum(qte) AS quantite, CASE 
         WHEN type_generique = 0 THEN 'Médicaments de marques'
@@ -68,3 +82,13 @@ GROUP BY     CASE
         WHEN type_generique = 0 THEN 'Médicaments de marques'
         WHEN type_generique IN (1, 2, 4) THEN 'Médicaments génériques'
     END
+
+
+
+CREATE VIEW comparaison_prix_medicaments
+AS
+SELECT SAE_S6_2025.cis.code_cis, denomination, prix_medicament_a, titulaires
+FROM SAE_S6_2025.cis
+INNER JOIN SAE_S6_2025.ciscip
+ON SAE_S6_2025.ciscip.code_cis = SAE_S6_2025.cis.code_cis
+WHERE prix_medicament_a != ''
