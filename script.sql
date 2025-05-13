94e4a0a439fed1bc6d152d5a0022b48245f280aa
antoninVeyre12 <antonin.veyre@iut-rodez.fr>
2025-05-13 15:31:14 +0200

    mise à jour d'une nouvelle vue

--- script.sql
+++ script.sql
@@ -92,3 +92,49 @@ FROM SAE_S6_2025.cis
 INNER JOIN SAE_S6_2025.ciscip
 ON SAE_S6_2025.ciscip.code_cis = SAE_S6_2025.cis.code_cis
 WHERE prix_medicament_a != ''
+
+CREATE VIEW dernieres_alertes_de_securite
+AS
+SELECT SAE_S6_2025.cis.denomination, date_fin, texte 
+FROM SAE_S6_2025.cisinfosimportantes
+INNER JOIN SAE_S6_2025.cis
+ON SAE_S6_2025.cis.code_cis = SAE_S6_2025.cisinfosimportantes.code_cis
+WHERE date_fin >= DATE_FORMAT(NOW() - INTERVAL 1 MONTH, '%Y-%m-01')     AND date_fin < DATE_FORMAT(NOW(), '%Y-%m-01')
+
+-- Vue pour l'affichage des resultats d'un medicament
+CREATE VIEW affichage_resultat_medicament
+AS 
+SELECT cis.code_cis, denomination, titulaires, forme_phamaceutique, voie_administration, cis.statut_administratif, etat_commercialisation, taux_remboursement, valeur_smr, GROUP_CONCAT(DISTINCT denomination_substance SEPARATOR '; ') AS substances, prix_medicament_b AS prix, reference_dosage, CASE 
+        WHEN type_generique = 0 THEN 'Médicaments de marques'
+        WHEN type_generique IN (1, 2, 4) THEN 'Médicaments génériques'
+        END AS type_medicament
+FROM cis
+LEFT JOIN ciscip 
+ON cis.code_cis = ciscip.code_cis
+LEFT JOIN cisgener
+ON cis.code_cis = cisgener.code_cis
+LEFT JOIN (
+    SELECT code_cis, GROUP_CONCAT(valeur_smr SEPARATOR '; ') AS valeurs_smr
+    FROM cishassmr
+    GROUP BY code_cis
+) AS smr ON cis.code_cis = smr.code_cis
+
+LEFT JOIN (
+    SELECT code_cis, GROUP_CONCAT(DISTINCT denomination_substance SEPARATOR '; ') AS substances
+    FROM ciscompo
+    GROUP BY code_cis
+) AS compo ON cis.code_cis = compo.code_cis
+
+GROUP BY 
+    cis.code_cis, 
+    denomination, 
+    titulaires, 
+    forme_phamaceutique, 
+    voie_administration, 
+    cis.statut_administratif, 
+    etat_commercialisation, 
+    taux_remboursement, 
+    valeur_smr, 
+    reference_dosage, 
+    type_generique,
+    prix;
